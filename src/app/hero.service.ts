bd030d3e9911b31602e33e5bce6b28d7eae3bd5c
lihw <MajyoArisu@outlook.com>
2018-04-30 21:07:24 +0800

    Tutral 5

---
+++ src/app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './heroe';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+}
